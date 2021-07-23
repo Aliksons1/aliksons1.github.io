b5143b82ebf6ae00a707498a6afd28657f4201b6
Aliksons1 <87674114+Aliksons1@users.noreply.github.com>
2021-07-23 23:24:32 +0100

    v1.2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var zmij = 0;
 
 var CPS = 0;
 
+
+
 function counter() {
 
 
@@ -12,17 +14,91 @@ function counter() {
 
 }
 
-function up1()
-
-{
+function up1() {
 
     var reqZmij = 50;
 
     if (reqZmij <= zmij) {
 
-        CPS = +10
+        CPS = CPS + 5
+
+        zmij = zmij - 50
+
 
         document.getElementById("cps").innerHTML = CPS;
+        document.getElementById("count").innerHTML = zmij;
+
+        if (CPS > 0) {
+
+            setInterval(function() {
+                zmij = (zmij + CPS);
+                document.getElementById("count").innerHTML = zmij;
+            }, 1000);
+
+        }
+
+    } else {
+
+        alert("Masz za mało żmijów!");
+
+    }
+
+}
+
+
+function up2() {
+
+    var reqZmij = 250;
+
+    if (reqZmij <= zmij) {
+
+        CPS = CPS + 15
+
+        zmij = zmij - 250
+
+
+        document.getElementById("cps").innerHTML = CPS;
+        document.getElementById("count").innerHTML = zmij;
+
+        if (CPS > 0) {
+
+            setInterval(function() {
+                zmij = (zmij + CPS);
+                document.getElementById("count").innerHTML = zmij;
+            }, 1000);
+
+        }
+
+    } else {
+
+        alert("Masz za mało żmijów!");
+
+    }
+
+}
+
+function up3() {
+
+    var reqZmij = 1000;
+
+    if (reqZmij <= zmij) {
+
+        CPS = CPS + 25
+
+        zmij = zmij - 1000
+
+
+        document.getElementById("cps").innerHTML = CPS;
+        document.getElementById("count").innerHTML = zmij;
+
+        if (CPS > 0) {
+
+            setInterval(function() {
+                zmij = (zmij + CPS);
+                document.getElementById("count").innerHTML = zmij;
+            }, 1000);
+
+        }
 
     } else {
 
