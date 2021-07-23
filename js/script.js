f97f2ec4aea670813517038c6ebd829ddf0992e8
Aliksons1 <87674114+Aliksons1@users.noreply.github.com>
2021-07-23 22:56:46 +0100

    v1.1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
-function counter {
+var zmij = 0;
 
-    var count = 0;
+var CPS = 0;
 
-    count++
+function counter() {
 
-    document.getElementById("count").innerHTML = count;
+
+
+    zmij++
+
+    document.getElementById("count").innerHTML = zmij;
+
+}
+
+function up1()
+
+{
+
+    var reqZmij = 50;
+
+    if (reqZmij <= zmij) {
+
+        CPS = +10
+
+        document.getElementById("cps").innerHTML = CPS;
+
+    } else {
+
+        alert("Masz za mało żmijów!");
+
+    }
 
 }
